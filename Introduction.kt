b3624794bae80f190e5451eb44b6adc95855ded0
Solferest <112902148+Solferest@users.noreply.github.com>
2024-02-19 13:36:57 +0300

    Add files via upload

---
+++ Introduction.kt
@@ -0,0 +1,75 @@
+fun start(): String {
+    return "OK"
+}
+
+fun main() {
+    val result = start()
+    print(result)
+}
+
+fun joinOptions(options: Collection<String>) =
+    options.joinToString(prefix = "[", postfix = "]", separator = ", ")
+
+fun main() {
+    val options = listOf("a", "b", "c")
+    val result = joinOptions(options)
+    println(result)
+}
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.uppercase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+const val question = "life, the universe, and everything"
+const val answer = 42
+
+val tripleQuotedString = """
+    #question = "$question"
+    #answer = $answer""".trimMargin("#")
+
+fun main() {
+    println(tripleQuotedString)
+}
+
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+fun getPattern(): String = """\d{2} $month \d{4}"""
+
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) {
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+import java.lang.IllegalArgumentException
+
+fun failWithWrongAge(age: Int?): Nothing {
+    throw IllegalArgumentException("Wrong age: $age")
+}
+
+fun checkAge(age: Int?) {
+    if (age == null || age !in 0..150) failWithWrongAge(age)
+    println("Congrats! Next year you'll be ${age + 1}.")
+}
+
+fun main() {
+    checkAge(10)
+}
+
+fun containsEven(collection: Collection<Int>): Boolean =
+        collection.any { it % 2 == 0}
